99008224b172ed87c737e0b1e73eea71b5fa05ef
Guinan Lin <guinanlin@192.168.0.105>
2018-09-14 00:48:30 +0800

    add string type pratices

---
+++ chapter4/4.10_strtype4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+
+    char charr[20];
+    string str;
+
+    cout << "Length of string in charr before input: " << strlen(charr) << endl;
+    cout << "Length of string in str before input: " << str.size() << endl;
+
+    cout << "Enter a line text:\n";
+    cin.getline(charr, 20);
+    cout << "You entered: " << charr << endl;
+
+    cout << "Enter another line text:\n";
+    getline(cin, str);
+    cout << "You entered: " << str << endl;
+
+    cout << "Length of string in charr after input: " << strlen(charr) << endl;
+    cout << "Length of string in str after input: " << str.size() << endl;
+
+    return 0;
+}
